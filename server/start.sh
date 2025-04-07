cd2dc66251128359a311b2bd9e5e1b333be2cbe1
Ahmet Talha Can <120933447+AhmetTalhaCan@users.noreply.github.com>
2025-04-07 04:17:04 +0300

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gunicorn -w 4 server.main:app --bind 0.0.0.0:443
+
+# Çevresel değişkenleri ayarla
+export FLASK_APP=server.main:app  # Flask uygulamanızın dosya adı (örneğin, server.main.py)
+export FLASK_ENV=production  # Flask ortamını üretim olarak ayarla
+
+# Render platformu için doğru portu kullan
+PORT=${PORT:-5000}  # Render, PORT değişkenini dinamik olarak sağlar, eğer tanımlı değilse 5000 kullan
+
+# Gunicorn ile Flask uygulamasını başlat
+gunicorn -w 4 server.main:app --bind 0.0.0.0:$PORT
